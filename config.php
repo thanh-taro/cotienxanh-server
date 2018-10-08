fad9eeb5bfa194c0889ec92814f5ec5ec9a1762d
Nguyen Tri Thanh <adamnguyen.itdn@gmail.com>
2018-10-08 22:43:01 +0700

    Add many features

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => false,
+        'db' => [
+            'host' => '127.0.0.1',
+            'dbname' => 'cotienxa_db',
+            'user' => 'cotienxa_admin',
+            'pass' => 'nguyensongnguyen'
+        ],
+    ],
+];
